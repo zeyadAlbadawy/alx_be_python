50bf6d71713986f9f0eeed3893efcbb1c0b53204
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-06-05 00:43:02 +0300

    The Second Task Done!

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,21 @@
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+
+opt = input("Choose the operation (+, -, *, /):").strip()
+
+match opt:
+    case "+":
+        print("The result is ", num1 + num2)
+    case "-":
+        print("The result is ", num1 - num2)
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            print("The result is", num1 / num2)
+    case "*":
+        print("The result is ", num1 * num2)
+    case _:
+        print("Invalid operation.")
+
+    
