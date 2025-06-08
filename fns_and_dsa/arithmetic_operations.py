5a190acf90c15ed4d04d0b6d53f6a5ca229aa1f0
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2025-06-08 23:49:11 +0300

    some changes done to the first problem

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
 def perform_operation(num1, num2, operation):
-    if(operation == 'add'):
+    if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
         return num1 - num2
     elif operation == 'multiply':
         return num1 * num2
-    else:
-        if(num2 == 0): 
+    elif operation == 'divide':
+        if num2 == 0: 
             return "The Devision By Zero is not Allowed"
         else:
             return num1 / num2
